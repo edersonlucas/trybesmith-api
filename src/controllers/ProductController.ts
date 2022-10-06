c47f9d4af3046b8df9c7fff11e71298b1a5f367f
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-06 19:40:57 -0300

    feat: Creates the product controller and implements the function to create the product

---
+++ src/controllers/ProductController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import IProduct from '../interfaces/ProductInterface';
+import ProductService from '../services/ProductService';
+
+export default class ProductController {
+  private service: ProductService;
+
+  constructor() {
+    this.service = new ProductService();
+  }
+  
+  public createProduct = async (req: Request<Omit<IProduct, 'Id, orderId'>>, res: Response) => {
+    const product = req.body;
+    const newProduct = await this.service.create(product);
+    return res.status(201).json(newProduct);
+  };
+}
