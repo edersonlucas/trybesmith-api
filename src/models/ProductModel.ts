4483be7c1e45108d83081b7ebe97bb4d8c28957d
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-06 19:30:20 -0300

    feat: Creates the product model and implements the create function

---
+++ src/models/ProductModel.ts
@@ -0,0 +1,21 @@
+import { Pool, OkPacket } from 'mysql2/promise';
+import IProduct from '../interfaces/ProductInterface';
+
+export default class ProductModel {
+  constructor(private connection: Pool) {
+    this.connection = connection;
+  }
+
+  async create(
+    product: Omit<IProduct, 'id, orderId'>,
+  ): Promise<number | null> {
+    const [{ insertId }] = await this.connection.execute<OkPacket>(
+      `INSERT INTO Trybesmith.Products
+          (name, amount)
+      VALUES
+          (?, ?)`,
+      [product.name, product.amount],
+    );
+    return insertId;
+  }
+}
