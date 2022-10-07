25d071a72a735744124d009262bd558cd9668278
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-06 23:23:13 -0300

    feat: Implement error checking if it contains the word "is required" returns a code 400 if not returns a 422

--- src/middlewares/validations/validationInputValues.ts
+++ src/middlewares/validations/validationInputValues.ts
@@ -5,7 +5,10 @@ import ErrorGenerator from '../../utils/ErrorGenerator';
 const validateCreateProduct = (newProduct: Omit<IProduct, 'id, orderId'>): void => {
   const { error } = createProductSchema
     .validate(newProduct);
-  if (error) throw new ErrorGenerator(400, error.message);
+  if (error) {
+    if (error.message.includes('is required')) throw new ErrorGenerator(400, error.message);
+    throw new ErrorGenerator(422, error.message);
+  }
 };
 
 export default validateCreateProduct;
