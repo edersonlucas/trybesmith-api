403d2ae643fa0fefbfbd5e7a623f52cf7a81c2c4
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-07 15:33:10 -0300

    refactor: Update error message using error generator

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,6 +1,7 @@
 import connection from '../models/connection';
 import ProductModel from '../models/ProductModel';
 import IProduct from '../interfaces/ProductInterface';
+import ErrorGenerator from '../utils/ErrorGenerator';
 
 export default class ProductService {
   private model: ProductModel;
@@ -11,10 +12,10 @@ export default class ProductService {
 
   public async createProduct(
     product: Omit<IProduct, 'id, orderId'>,
-  ): Promise<Omit<IProduct, 'orderId'> | Error> {
+  ): Promise<Omit<IProduct, 'orderId'> | ErrorGenerator> {
     const newProductId = await this.model.createProduct(product);
     if (newProductId) return { ...product, id: newProductId };
-    throw new Error('Unable to register new product');
+    throw new ErrorGenerator(409, 'Unable to register new product');
   }
 
   public async getAllProducts(): Promise<IProduct[]> {
