234de1054dc7c17acbe3e7c0157f75cd0ab76404
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-07 16:21:59 -0300

    feat: Creates the order controller and implement the getAllOrders function

---
+++ src/controllers/OrderController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import IOrder from '../interfaces/OrderInterface';
+import OrderService from '../services/OrderService';
+
+export default class OrderController {
+  private service: OrderService;
+
+  constructor() {
+    this.service = new OrderService();
+  }
+
+  public getAllOrders = async (req: Request, res: Response<IOrder[]>) => {
+    const orders = await this.service.getAllOrders();
+    return res.status(200).json(orders);
+  };
+}
