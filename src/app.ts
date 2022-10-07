3f591a9479b44f5b836b31f0abf04d1aecc811b8
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-06 22:06:09 -0300

    feat: Implements error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import errorMiddleware from './middlewares/errorMiddleware';
 import productRouter from './routes/productRouter';
 
 const app = express();
 
 app.use(express.json());
 app.use(productRouter);
+app.use(errorMiddleware);
 
 export default app;
