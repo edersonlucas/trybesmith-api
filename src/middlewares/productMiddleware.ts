e955a8fc63855e96c3abe8d6079c320e89531315
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-06 22:02:34 -0300

    feat: Create product middleware

---
+++ src/middlewares/productMiddleware.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from 'express';
+import IProduct from '../interfaces/ProductInterface';
+import validateCreateProduct from './validations/validationInputValues';
+
+export default (
+  req: Request<Omit<IProduct, 'Id, orderId'>>,
+  res: Response,
+  next: NextFunction,
+) => {
+  validateCreateProduct(req.body);
+  next();
+};
