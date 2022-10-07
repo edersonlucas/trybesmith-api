0dd7ec1ad3e20bc5b04e797a7a87f763fe3f73f4
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-07 15:46:26 -0300

    feat: Create user validation for middleware

--- src/middlewares/validations/validationInputValues.ts
+++ src/middlewares/validations/validationInputValues.ts
@@ -1,6 +1,7 @@
-import createProductSchema from './schemas';
+import { createProductSchema, createUserSchema } from './schemas';
 import IProduct from '../../interfaces/ProductInterface';
 import ErrorGenerator from '../../utils/ErrorGenerator';
+import IUser from '../../interfaces/UserInterface';
 
 const validateCreateProduct = (newProduct: Omit<IProduct, 'id, orderId'>): void => {
   const { error } = createProductSchema
@@ -11,4 +12,13 @@ const validateCreateProduct = (newProduct: Omit<IProduct, 'id, orderId'>): void 
   }
 };
 
-export default validateCreateProduct;
+const validateCreateUser = (newUser: Omit<IUser, 'id'>): void => {
+  const { error } = createUserSchema
+    .validate(newUser);
+  if (error) {
+    if (error.message.includes('is required')) throw new ErrorGenerator(400, error.message);
+    throw new ErrorGenerator(422, error.message);
+  }
+};
+
+export { validateCreateProduct, validateCreateUser };
