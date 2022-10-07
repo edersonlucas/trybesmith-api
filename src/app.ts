d872739713723bb106a26fdaec1742da958a995e
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-07 18:37:33 -0300

    feat: Implements the login route in the app

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import errorMiddleware from './middlewares/errorMiddleware';
 import productRouter from './routes/productRouter';
 import userRouter from './routes/userRouter';
 import orderRouter from './routes/orderRouter';
+import loginRouter from './routes/loginRouter';
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(productRouter);
 app.use(userRouter);
 app.use(orderRouter);
+app.use(loginRouter);
 app.use(errorMiddleware);
 
 export default app;
