4786e92f5f5425958a628d3ca233f0ad4cab76aa
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-07 15:49:13 -0300

    feat: Implements the user route in the app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import errorMiddleware from './middlewares/errorMiddleware';
 import productRouter from './routes/productRouter';
+import userRouter from './routes/userRouter';
 
 const app = express();
 
 app.use(express.json());
 app.use(productRouter);
+app.use(userRouter);
 app.use(errorMiddleware);
 
 export default app;
