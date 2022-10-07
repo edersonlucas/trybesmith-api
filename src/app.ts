4817cd851937447fa4ce0d35b62d4d82a87d647e
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-07 16:23:06 -0300

    feat: Implements the order route in the app

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import errorMiddleware from './middlewares/errorMiddleware';
 import productRouter from './routes/productRouter';
 import userRouter from './routes/userRouter';
+import orderRouter from './routes/orderRouter';
 
 const app = express();
 
 app.use(express.json());
 app.use(productRouter);
 app.use(userRouter);
+app.use(orderRouter);
 app.use(errorMiddleware);
 
 export default app;
