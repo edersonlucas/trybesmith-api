865f4a607fb5bbf98b3f93575a50edb9e9fef621
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-07 16:17:56 -0300

    feat: Creates the order model

---
+++ src/models/OrderModel.ts
@@ -0,0 +1,17 @@
+import { Pool, RowDataPacket } from 'mysql2/promise';
+import IOrder from '../interfaces/OrderInterface';
+
+export default class OrderModel {
+  constructor(private connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async getAllOrders(): Promise<IOrder[]> {
+    const [orders] = await this.connection.execute<(IOrder & RowDataPacket)[]>(
+      `SELECT A.id, A.userId, JSON_ARRAYAGG(B.id) as productsIds 
+      FROM Trybesmith.Orders as A 
+      JOIN Trybesmith.Products as B 
+      ON A.id = B.orderId GROUP BY A.id`);
+    return orders;
+  }
+}
