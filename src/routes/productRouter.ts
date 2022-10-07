12016aee7d79a3945d5b955e96f187c4146b40fe
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-06 22:04:02 -0300

    feat: Implements validation middleware in POST /products route

--- src/routes/productRouter.ts
+++ src/routes/productRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import ProductController from '../controllers/ProductController';
+import productMiddleware from '../middlewares/productMiddleware';
 
 const router = Router();
 
 const productController = new ProductController();
 
-router.post('/products', productController.createProduct);
+router.post('/products', productMiddleware, productController.createProduct);
 
 export default router;
