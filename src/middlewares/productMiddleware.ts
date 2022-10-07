3ec341f17dc3cb29f9371254dfa9979a8b5b03a1
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-07 15:47:40 -0300

    refactor:  Updates import of validateCreateProduct

--- src/middlewares/productMiddleware.ts
+++ src/middlewares/productMiddleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import IProduct from '../interfaces/ProductInterface';
-import validateCreateProduct from './validations/validationInputValues';
+import { validateCreateProduct } from './validations/validationInputValues';
 
 export default (
   req: Request<Omit<IProduct, 'Id, orderId'>>,
