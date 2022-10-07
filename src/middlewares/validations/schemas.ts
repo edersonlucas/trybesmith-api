5cc35752a443f42ad7040f79625196fff38ee7fc
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-06 23:20:56 -0300

    feat: Implements checking if name and amount are of type string and have at least 3 characters

--- src/middlewares/validations/schemas.ts
+++ src/middlewares/validations/schemas.ts
@@ -1,8 +1,8 @@
 import Joi from 'joi';
 
 const createProductSchema = Joi.object({
-  name: Joi.required(),
-  amount: Joi.required(),
+  name: Joi.string().min(3).required(),
+  amount: Joi.string().min(3).required(),
 });
 
 export default createProductSchema; 
