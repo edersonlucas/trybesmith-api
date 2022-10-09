dde856a88ee02739a3236387b52cf52c97cdf3b3
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-08 21:08:39 -0300

    feat: Creates the method to create an order in the controller

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from 'express';
 import IOrder from '../interfaces/OrderInterface';
+import IUser from '../interfaces/UserInterface';
 import OrderService from '../services/OrderService';
 
 require('express-async-errors');
 
+interface CreateOrderRequest extends Request {
+  user?: IUser
+}
 export default class OrderController {
   private service: OrderService;
-
+  
   constructor() {
     this.service = new OrderService();
   }
@@ -14,5 +18,17 @@ export default class OrderController {
   public getAllOrders = async (req: Request, res: Response<IOrder[]>) => {
     const orders = await this.service.getAllOrders();
     return res.status(200).json(orders);
+  };
+
+  public createOrder = async (
+    req: CreateOrderRequest,
+    res: Response,
+  ) => {
+    const { productsIds } = req.body;
+    const userId = req.user?.id;
+    if (userId) {
+      const newOrder = await this.service.createOrder(userId, productsIds);
+      return res.status(201).json(newOrder);
+    }
   };
 }
