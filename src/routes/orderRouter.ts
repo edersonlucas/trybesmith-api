bb642c8b39eac20fadfd5dbdb1138219627318d4
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-08 21:18:51 -0300

    feat: creates POST /orders route in orderRouter

--- src/routes/orderRouter.ts
+++ src/routes/orderRouter.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import OrderController from '../controllers/OrderController';
+import orderMiddleware from '../middlewares/orderMiddleware';
+import authorizationMiddleware from '../middlewares/authorizationMiddleware';
 
 const router = Router();
 
 const orderController = new OrderController();
 
 router.get('/orders', orderController.getAllOrders);
+router.post('/orders', authorizationMiddleware, orderMiddleware, orderController.createOrder);
 
 export default router;
